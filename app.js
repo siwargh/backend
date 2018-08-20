33dc2a579ba3f0b05c616452676bdd55d5bdd5db
Siwar Ghammamm <sousousiwar913@gmail.com>
2018-08-20 12:00:49 +0100

    changes

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyparser=require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var a="myapp";
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
