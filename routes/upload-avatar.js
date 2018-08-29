4dcd7d29adf7f055a3c08177f2f6c391d558a2dd
feridHelali <helaliferid@gmail.com>
2018-08-29 13:15:07 +0100

    add upload multer

---
+++ routes/upload-avatar.js
@@ -0,0 +1,51 @@
+var express =require('express');
+var multer=require('multer');
+var Q =require('q');
+var router=express.Router();
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function(req, file, cb) {
+        cb(null, './uploads/avatars');
+    },
+    filename: function(req, file, cb) {
+        var userId = req.body.userId;
+        var path = file.fieldname + '-' + userId + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1];
+        cb(null, path);
+
+        updateAvatar(userId, path)
+            .then(function() {
+                res.sendStatus(200);
+            })
+            .catch(function(err) {
+                res.status(400).send(err);
+            });
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('avatar');
+
+function updateAvatar(_id, path) {
+    var deferred = Q.defer();
+    User.findByIdAndUpdate(_id, { $set: { avatar_url: path } },
+        function(err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            deferred.resolve();
+        });
+    return deferred.promise;
+}
+
+router.post('/upload', upload, function(req, res, next) {
+    upload(req, res, function(err) {
+        if (err) {
+            res.json({ error_code: 1, err_desc: err });
+            return;
+        }
+        res.json({ error_code: 0, err_desc: 'Avatar successfully uploded' });
+    });
+});
+
+module.exports = router;
+
+
